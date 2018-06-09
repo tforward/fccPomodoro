f8748a458e0527d2906348086384f8d9db8dd365
Tristan Forward <tforward@users.noreply.github.com>
2018-06-09 14:54:22 -0400

    display fixed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -34,20 +34,52 @@ myBase.main = function main(id) {
 
   if (id === "start_stop") {
     startStop(self);
+  } else if (id === "session-increment") {
+    num = upperLimit(num);
+    setSessionLength(num);
+    formattedDisplayTime(num);
+    reset();
+  } else if (id === "session-decrement") {
+    num = lowerLimit(num);
+    setSessionLength(num);
+    formattedDisplayTime(num);
+    reset(num);
+  } else if (id === "break-increment") {
+    numBreak = upperLimit(numBreak);
+    setBreakLength(numBreak);
+  } else if (id === "break-decrement") {
+    numBreak = lowerLimit(numBreak);
+    setBreakLength(numBreak);
   } else if (id === "reset") {
     reset();
-  } else if (id === "session-increment") {
-    num += 1;
-  } else if (id === "session-decrement") {
-    num -= 1;
-  } else if (id === "break-increment") {
-    numBreak += 1;
-  } else if (id === "break-decrement") {
-    numBreak -= 1;
+    setSessionLength(25);
+    setBreakLength(5);
+    formattedDisplayTime(25);
   }
-  setSessionLength(num);
-  setBreakLength(numBreak);
 };
+
+function formattedDisplayTime(num) {
+  const mmss = getformatSessionLength(num);
+  displayTime(mmss);
+}
+
+function upperLimit(num) {
+  let limit = num;
+  if (limit > 60) {
+    return limit;
+  }
+  limit += 1;
+  return limit;
+}
+
+function lowerLimit(num) {
+  let limit = num;
+  if (limit <= 1) {
+    return limit;
+  }
+  limit -= 1;
+  return limit;
+}
 
 function reset() {
   if (myApp.timer !== undefined) {
@@ -56,6 +88,7 @@ function reset() {
     const startBtn = myApp.obj["start_stop"];
     startBtn.toggle = -1;
     startBtn.elem.className = "fas fa-play-circle";
+    clockDial(0, 0);
   }
 }
 
@@ -73,12 +106,19 @@ function startStop(obj) {
     self.toggle = 0;
     myApp.timer.pause();
     self.elem.className = "fas fa-play-circle";
+    const pausedTime = myApp.timer.final_time * 0.001;
+    const x = pausedTime / 60;
+    const b = getSessionLength() - x;
+    const c = getSessionLength() / b;
+    const d = 60 / c;
+    clockDial(d, 0);
   } else {
     // Start
     self.toggle = 1;
     // Unpause
     myApp.timer.pause();
     self.elem.className = "fas fa-pause-circle";
+    clockDial(60, myApp.timer.final_time * 0.001);
   }
 }
 
@@ -100,21 +140,19 @@ function getSessionLength() {
   return sessionAsStr;
 }
 
-function getformatSessionLength() {
-  const sessionAsStr = getSessionLength();
-  return `${sessionAsStr}:00`;
-}
-
 function timer() {
   const session = getformatSessionLength();
   const startTime = myApp.timer.timeToMS(session);
 
-  setClockDisplay(60);
-
   // Get the time in mins and convert to seconds
   const sessionLengthSecs = myApp.obj["session-length"].elem.textContent * 60;
-  setTimeDuration(sessionLengthSecs);
+  clockDial(60, sessionLengthSecs);
   return startTime;
+}
+
+function clockDial(clock, session) {
+  setClockDisplay(clock);
+  setTimeDuration(session);
 }
 
 function addElements() {
@@ -145,21 +183,32 @@ function ButtonDelegator(proto = null) {
   Button.setup = function setup() {
     this.toggle = -1;
   };
-
   return Button;
 }
 
-function displayTime() {
+function getformatSessionLength() {
+  const sessionAsStr = getSessionLength();
+  return `${sessionAsStr}:00`;
+}
+
+function displayTime(timeMMSS) {
+  const str = `${timeMMSS}`;
+  const pad = "00000";
+  const formatted = pad.substring(0, pad.length - str.length) + str;
+  myApp.obj["time-left"].elem.textContent = formatted;
+}
+
+function updateTime() {
   const currentTime = myApp.timer.msToTimecode(myApp.timer.lap());
   const timeMMSS = currentTime.substring(3, 8);
-  myApp.obj["time-left"].elem.textContent = timeMMSS;
+  displayTime(timeMMSS);
 }
 
 function getTimer() {
   const newTimer = new Tock({
     countdown: true,
     interval: 250,
-    callback: displayTime
+    callback: updateTime
     // complete: someCompleteFunction
   });
   return newTimer;
